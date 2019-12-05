f73a697fa0b5ac7eaed033c24a57274c226db5ce
ji4xi <saltycupofsoda@gmail.com>
2019-12-04 16:19:25 -0800

    Added an autonomous where the robot waits 25 seconds then goes forward when it seems like the end in inendiehviwinv inevidable

---
+++ SkyStone-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/Experiments/Jiaxi/Park.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.robotcontroller.internal.Experiments.Jiaxi;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.robotcontroller.internal.RobotPrograms.SkyScraper;
+import org.firstinspires.ftc.robotcontroller.internal.RobotPrograms.SkyScraperIntakeBlue;
+
+@Autonomous
+public class Park extends SkyScraper {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        super.runOpMode();
+        sleep(25000);
+        moveForwardInchesGyro(10, 0, 3);
+    }
+}
