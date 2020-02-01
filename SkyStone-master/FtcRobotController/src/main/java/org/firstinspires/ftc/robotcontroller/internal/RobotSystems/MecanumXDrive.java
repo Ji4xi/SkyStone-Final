64b7bbdfe8a91259d6123272d565b35bb2e89cbb
chawei217 <michaelwei0606@gmail.com>
2020-01-31 16:20:16 -0800

    mike 4:30'

--- SkyStone-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RobotSystems/MecanumXDrive.java
+++ SkyStone-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RobotSystems/MecanumXDrive.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import org.firstinspires.ftc.robotcontroller.internal.TestProps.MecanumX;
 
 public class MecanumXDrive extends DriveTrain {
+    public Boolean dir = true;
     public MecanumXDrive() {}
     public void telemetry(){
         telemetry.addData("MECANUM X DRIVE", "TELEMETRY");
@@ -15,10 +16,18 @@ public class MecanumXDrive extends DriveTrain {
     }
 
     public void update(){
-        motors.get(0).setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x * turnPwrMax) * drivePwrMax); //-
-        motors.get(1).setPower((-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x * turnPwrMax) * drivePwrMax); //+
-        motors.get(2).setPower((-gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x * turnPwrMax) * drivePwrMax); //+
-        motors.get(3).setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x * turnPwrMax) * drivePwrMax); //-
+        if (dir) {
+            motors.get(0).setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x * turnPwrMax) * drivePwrMax); //-
+            motors.get(1).setPower((-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x * turnPwrMax) * drivePwrMax); //+
+            motors.get(2).setPower((-gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x * turnPwrMax) * drivePwrMax); //+
+            motors.get(3).setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x * turnPwrMax) * drivePwrMax); //-
+        } else {
+            motors.get(0).setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x * turnPwrMax) * drivePwrMax); //-
+            motors.get(1).setPower((-gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x * turnPwrMax) * drivePwrMax); //+
+            motors.get(2).setPower((-gamepad1.left_stick_y + gamepad1.left_stick_x + gamepad1.right_stick_x * turnPwrMax) * drivePwrMax); //+
+            motors.get(3).setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x * turnPwrMax) * drivePwrMax); //-
+        }
+
     }
 
     @Override
@@ -49,6 +58,7 @@ public class MecanumXDrive extends DriveTrain {
                 if (i % 2 == 0) motors.get(i).setDirection(DcMotorSimple.Direction.FORWARD);
                 else motors.get(i).setDirection(DcMotorSimple.Direction.REVERSE);
             }
+            dir = false;
         }
     }
 }
