f7f0f40a34ec80416659b41bf03cecef5abc45ba
ji4xi <saltycupofsoda@gmail.com>
2020-02-04 14:12:47 -0800

    Jiaxi 2/4

--- SkyStone-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RobotPrograms/MichaelBot/Park.java
+++ SkyStone-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RobotPrograms/MichaelBot/Park.java
@@ -1,10 +1,13 @@
 package org.firstinspires.ftc.robotcontroller.internal.RobotPrograms.MichaelBot;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous
 public class Park extends MichaelBot {
     @Override
     public void runOpMode() throws InterruptedException {
         super.runOpMode();
-        sleep(28500);
+        sleep(27000);
         XLinearInchesGyro(19, Side.LEFT, 0.7, 0);
     }
 }
