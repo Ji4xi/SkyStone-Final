f189ea57663c5112e35db69e97edf8a663f6ad47
chawei217 <michaelwei0606@gmail.com>
2020-03-04 18:34:53 -0800

    mike

--- SkyStone-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RobotPrograms/Odometry/FinalSkystoneBlue.java
+++ SkyStone-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RobotPrograms/Odometry/FinalSkystoneBlue.java
@@ -1,7 +1,11 @@
 package org.firstinspires.ftc.robotcontroller.internal.RobotPrograms.Odometry;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 import org.firstinspires.ftc.robotcontroller.internal.RobotPrograms.StoneFinder.NewStoneFinder;
 
+@Autonomous
 public class FinalSkystoneBlue extends NewStoneFinder {
     int count = 0;
 
@@ -15,55 +19,41 @@ public class FinalSkystoneBlue extends NewStoneFinder {
         //top
         double skystone_pos;
         if (skystoneAngle < 0) {
-            skystone_pos = -(SKYSTONE_INCH * 1 + 10);
+            skystone_pos = -(SKYSTONE_INCH * 0 + 10);
         } else if (skystoneAngle == 0) {
-            skystone_pos = -(SKYSTONE_INCH * 2 + 10);
+            skystone_pos = -(SKYSTONE_INCH * 1 + 10);
         } else {
             skystone_pos = -(SKYSTONE_INCH * 2 + 10);
         }
 
-        goToPositionSupreme(skystone_pos, TILE_INCH + 6, maxPwr, 90, 1, 0.00114, 0, 0);
-        if (skystoneAngle == 0) {
-            skystone_pos = -(SKYSTONE_INCH * 3 + 10);
-
-        }
-
-
-//
-//        //open up
+        goToPositionSupreme(skystone_pos, TILE_INCH + 2.7, maxPwr, 90, 1, 0.00106, 0, 0);
 
         if (skystoneAngle < 0) {
             turnPID(0, Direction.CLOCKWISE, maxPwr, 0.0180, 0.01, 0.001);
         } else {
-
             turnPID(0, Direction.CLOCKWISE, maxPwr, 0.0180, 0.01, 0.001);
-
         }
 
-        if (skystoneAngle == 0) {
-            goToPositionSupreme(skystone_pos, TILE_INCH + 6, maxPwr, 0, 1, 0.0025, 0, 0);
-
-        }
 
         topClaw.setPosition(0.16);
         bottomClaw.setPosition(0.617);
 
-        goToPositionSupreme(skystone_pos, TILE_INCH + 10.5, maxPwr, 0, 1,0.0025, 0.0, 0);
+        goToPositionSupreme(skystone_pos, TILE_INCH + 6.2, maxPwr, 0, 1,0.00253, 0.0, 0);
         topClaw.setPosition(0.855);
         sleep(300);
         bottomClaw.setPosition(0.166);
-        goToPositionSupreme(skystone_pos, TILE_INCH + 3.5, maxPwr, 0, 1,0.0019, 0.0, 0);
+        goToPositionSupreme(skystone_pos, TILE_INCH + 0.5, maxPwr, 0, 1,0.002, 0.0, 0);
         if (skystoneAngle == 0) {
-            goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+ 4 * TILE_INCH-7, TILE_INCH + 2.8, 0.9, 0, 1.55,0.00155, 0, 0.00034); //d: 0.00033
+            goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+ 4 * TILE_INCH-7 + 25.5 - 8, TILE_INCH + 2.5, 0.9, 0, 1.55,0.00151, 0, 0.00034); //d: 0.00033
         } else {
-            goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+ 4 * TILE_INCH-7, TILE_INCH + 4.8, 0.9, 0, 1.55,0.00155, 0, 0.00034); //d: 0.00033
+            goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+ 4 * TILE_INCH-7 + 25.5 - 8, TILE_INCH + 2.5, 0.9, 0, 1.55,0.00151, 0, 0.00034); //d: 0.00033
         }
 
-        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10) + 4 * TILE_INCH-5, TILE_INCH + 11.5, maxPwr, 0, 1.3,0.0023, 0.0, 0.0001);
+        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+ 4 * TILE_INCH-7 + 25.5 - 8, TILE_INCH + 9.3, maxPwr, 0, 1.3,0.0021, 0.0, 0.0001); //d: 0.00033
         bottomClaw.setPosition(0.500);
         sleep(100);
         topClaw.setPosition(0.2329);
-        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10) + 4 * TILE_INCH-7, TILE_INCH + 7, maxPwr, 0, 1,0.0022, 0.0, 0);
+        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10) + 4 * TILE_INCH-7 + 25.5 - 8, TILE_INCH + 5, maxPwr, 0, 1.25,0.0023, 0.0, 0);
 //
 //        goToPositionHard(skystone_pos + 4 * TILE_INCH-4, TILE_INCH + 8, maxPwr-0.6, 0, 1);
         bottomClaw.setPosition(0.21);
@@ -71,16 +61,16 @@ public class FinalSkystoneBlue extends NewStoneFinder {
 
 //
         skystone_pos+=24;
-        goToPositionSupreme(skystone_pos - 1, TILE_INCH + 6, 0.9, 0,1.5,0.00165, 0, 0.00039); //d:0.00042
+        goToPositionSupreme(skystone_pos - 1 + 2, TILE_INCH + 1.5, 0.9, 0,1.5,0.00151, 0, 0.00039); //d:0.000155
         topClaw.setPosition(0.16);
         bottomClaw.setPosition(0.617);
-        goToPositionSupreme(skystone_pos - 3, TILE_INCH + 12.1, maxPwr, 0, 1,0.0020, 0.0, 0);
+        goToPositionSupreme(skystone_pos - 1 + 2 - 1, TILE_INCH + 7.1, maxPwr, 0, 1.2,0.0018, 0.0, 0);
         topClaw.setPosition(0.855);
         sleep(300);
         bottomClaw.setPosition(0.166);
-        goToPositionSupreme(skystone_pos - 1, TILE_INCH + 3.5, maxPwr, 0, 1.31,0.00164, 0.0, 0); //0.0017
-        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+24 + 4 * TILE_INCH -7 + 12-24.5, TILE_INCH + 6, 0.9, 0, 1.5,0.0015, 0, 0.00025);
-        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+24 + 4 * TILE_INCH -7 + 12-24.5, TILE_INCH + 13.3, maxPwr, 0, 1.2,0.0018, 0.0, 0);//0.0020
+        goToPositionSupreme(skystone_pos - 1 + 2 - 1, TILE_INCH + 0, maxPwr, 0, 1.35,0.00164, 0.0, 0); //0.0017
+        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+ 4 * TILE_INCH-7 + 25.5 - 8 + 10, TILE_INCH + 6, 0.9, 0, 1.7,0.00135, 0, 0.00025);
+        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+ 4 * TILE_INCH-7 + 25.5 - 8 + 10, TILE_INCH + 13.5, maxPwr, 0, 1.3,0.0020, 0.0, 0);//0.0020
         bottomClaw.setPosition(0.500);
         sleep(100);
         topClaw.setPosition(0.2329);
@@ -88,16 +78,16 @@ public class FinalSkystoneBlue extends NewStoneFinder {
         turnPID(270, Direction.CLOCKWISE, maxPwr, 0.019, 0.01, 0.001);
         topClaw.setPosition(0.16);
         bottomClaw.setPosition(0.617);
-        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+24 + 4 * TILE_INCH-7+12-24, 47.25-17.5+12, maxPwr, 270, 1,0.0019,0,0.0001);
+        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+ 4 * TILE_INCH-7 + 25.5 - 8 + 10, 47.25-17.5+11.8, maxPwr, 270, 1.7,0.00195,0,0.0001);
         //foundation
         ls.setPosition(0.5);
         rs.setPosition(0.5);
         sleep(300);
-        goToPositionSupreme(TILE_INCH*5-17.5-2, 0.5, 0.9, 270, 1.9,0.0020, 0.0, 0.00);
+        goToPositionSupreme(TILE_INCH*5-17.5-2, 0.3, 0.9, 270, 1.9,1.4);
         ls.setPosition(0);
         rs.setPosition(0);
         sleep(100);
-        goToPositionSupreme(TILE_INCH*2-17.5, 0.7, 0.9, 270, 1.5);
+        goToPositionSupreme(TILE_INCH*2-17.5, -0.5, 0.9, 270, 1.5);
 
     }
     public void deliver(double skystone_pos, double offset) throws InterruptedException{
@@ -131,5 +121,79 @@ public class FinalSkystoneBlue extends NewStoneFinder {
 
     public double reposX(double originalX) {
         return - (originalX - 5.7);
+    }
+
+    @Override
+    public void goToPositionSupreme(double targetXPosition, double targetYPosition, double robotPower, double gyroAngle, double allowableDistanceError, double p, double i , double d, double gyroPwr) {
+        targetXPosition = reposX(targetXPosition);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        double circ = Math.PI * (3.93701);
+        targetXPosition = mode ? targetXPosition : -targetXPosition;
+
+        double originalP = pd.getP();
+        double originalI = pd.getI();
+        double originalD = pd.getD();
+
+        pd.setPID(p, i, d);
+        double angle;
+        double distanceToXTarget = targetXPosition * COUNTS_PER_INCH - globalCoordinate.getGlobalX();
+        double distanceToYTarget = targetYPosition * COUNTS_PER_INCH - globalCoordinate.getGlobalY();
+        double snipPwr = robotPower;
+        double distance = Math.hypot(distanceToXTarget, distanceToYTarget);
+        double target = distance;
+
+        //for telemetry purpose
+        double initialXPos = globalCoordinate.getGlobalX();
+        double initialYPos = globalCoordinate.getGlobalY();
+        double XOvershoot = 0;
+        double YOvershoot = 0;
+
+        fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        pd.initPD(target);
+
+        while (opModeIsActive() && distance > allowableDistanceError * COUNTS_PER_INCH) {
+            double globalX = globalCoordinate.getGlobalX();
+            double globalY = globalCoordinate.getGlobalY();
+            double normalHeading = getNormalizedHeading();
+            distanceToXTarget = targetXPosition * COUNTS_PER_INCH - globalX;
+            distanceToYTarget = targetYPosition * COUNTS_PER_INCH - globalY;
+            angle = Math.toDegrees(Math.atan2(distanceToYTarget, distanceToXTarget));
+            distance = Math.hypot(distanceToXTarget, distanceToYTarget);
+
+            snipPwr = Math.abs(pd.actuator(target - distance)) * robotPower;
+
+            fr.setPower(transformation((angle) + 90 - normalHeading, "fr") * snipPwr + gyro(gyroAngle, "fr") * (gyroPwr));
+            fl.setPower(transformation((angle) + 90 - normalHeading, "fl") * snipPwr + gyro(gyroAngle, "fl") * (gyroPwr));
+            br.setPower(transformation((angle) + 90 - normalHeading, "br") * snipPwr + gyro(gyroAngle, "br") * (gyroPwr));
+            bl.setPower(transformation((angle) + 90 - normalHeading, "bl") * snipPwr + gyro(gyroAngle, "bl") * (gyroPwr));
+
+            //overshoot
+            if (globalY - targetYPosition < 0 && targetYPosition - initialYPos < 0) YOvershoot = Math.abs(globalY - targetYPosition) > YOvershoot ? Math.abs(globalY - targetYPosition) : YOvershoot;
+            else if (globalY - targetYPosition > 0 && targetYPosition - initialYPos > 0) YOvershoot = Math.abs(globalY - targetYPosition) > YOvershoot ? Math.abs(globalY - targetYPosition) : YOvershoot;
+
+            if (globalX - targetXPosition < 0 && targetXPosition - initialXPos < 0) XOvershoot = Math.abs(globalX - targetXPosition) > XOvershoot ? Math.abs(globalX - targetXPosition) : XOvershoot;
+            else if (globalX - targetXPosition > 0 && targetXPosition - initialXPos > 0) XOvershoot = Math.abs(globalX - targetXPosition) > XOvershoot ? Math.abs(globalX - targetXPosition) : XOvershoot;
+
+            Object[] names = {"fr", "fl", "br", "bl","p_contr", "i_contr", "d_contr", "dis_left_x", "dis_left_y"};
+            Object[] values = {fr.getCurrentPosition(), fl.getCurrentPosition(), br.getCurrentPosition(), bl.getCurrentPosition(),pd.getPContrb(), pd.getIContrb(), pd.getDContrb(), distanceToXTarget / COUNTS_PER_INCH, distanceToYTarget / COUNTS_PER_INCH};
+
+            telemetry(names, values);
+        }
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fl.setPower(0);
+        fr.setPower(0);
+        bl.setPower(0);
+        br.setPower(0);
+        pd.setPID(originalP, originalI, originalD);
     }
 }
--- SkyStone-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RobotPrograms/Odometry/FinalSkystoneRed.java
+++ SkyStone-master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/RobotPrograms/Odometry/FinalSkystoneRed.java
@@ -32,7 +32,7 @@ public class FinalSkystoneRed extends NewStoneFinder {
             skystone_pos = -(SKYSTONE_INCH * 2 + 10);
         }
 
-        goToPositionSupreme(skystone_pos, TILE_INCH + 6, maxPwr, 90, 1, 0.00104, 0, 0);
+        goToPositionSupreme(skystone_pos, TILE_INCH + 6, maxPwr, 90, 1, 0.00100, 0, 0);
         if (skystoneAngle == 0) {
             skystone_pos = -(SKYSTONE_INCH * 3 + 10);
 
@@ -51,7 +51,7 @@ public class FinalSkystoneRed extends NewStoneFinder {
         }
 
         if (skystoneAngle == 0) {
-            goToPositionSupreme(skystone_pos, TILE_INCH + 6, maxPwr, 0, 1.3, 0.00262, 0, 0);
+            goToPositionSupreme(skystone_pos, TILE_INCH + 6, 0.7, 0, 1.3, 0.00262, 0, 0);
 
         }
 
@@ -69,7 +69,7 @@ public class FinalSkystoneRed extends NewStoneFinder {
             goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+ 4 * TILE_INCH-7, TILE_INCH + 4.8, 0.9, 0, 1.55,0.00155, 0, 0.00034); //d: 0.00033
         }
 
-        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10) + 4 * TILE_INCH-5, TILE_INCH + 11.5, maxPwr, 0, 1.3,0.0023, 0.0, 0.0001);
+        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10) + 4 * TILE_INCH-5, TILE_INCH + 11.5, maxPwr, 0, 1.3,0.0021, 0.0, 0.0001);
         bottomClaw.setPosition(0.500);
         sleep(100);
         topClaw.setPosition(0.2329);
@@ -81,7 +81,7 @@ public class FinalSkystoneRed extends NewStoneFinder {
 
 //
         skystone_pos+=24;
-        goToPositionSupreme(skystone_pos - 1, TILE_INCH + 6, 0.9, 0,1.5,0.00155, 0, 0.00039); //d:0.00042
+        goToPositionSupreme(skystone_pos - 1, TILE_INCH + 6, 0.9, 0,1.5,0.00158, 0, 0.00039); //d:0.00155
         topClaw.setPosition(0.16);
         bottomClaw.setPosition(0.617);
         goToPositionSupreme(skystone_pos - 3, TILE_INCH + 12.1, maxPwr, 0, 1,0.0020, 0.0, 0);
@@ -89,7 +89,7 @@ public class FinalSkystoneRed extends NewStoneFinder {
         sleep(300);
         bottomClaw.setPosition(0.166);
         goToPositionSupreme(skystone_pos - 1, TILE_INCH + 3.5, maxPwr, 0, 1.35,0.00164, 0.0, 0); //0.0017
-        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+24 + 4 * TILE_INCH -7 + 12-22.5, TILE_INCH + 6, 0.9, 0, 1.5,0.0015, 0, 0.00025);
+        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+24 + 4 * TILE_INCH -7 + 12-22.5, TILE_INCH + 6, 0.9, 0, 1.7,0.00143, 0, 0.00025); //0.00140
         goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+24 + 4 * TILE_INCH -7 + 12-22.5, TILE_INCH + 12.5, maxPwr, 0, 1.2,0.0018, 0.0, 0);//0.0020
         bottomClaw.setPosition(0.500);
         sleep(100);
@@ -98,7 +98,7 @@ public class FinalSkystoneRed extends NewStoneFinder {
         turnPID(270, Direction.CLOCKWISE, maxPwr, 0.019, 0.01, 0.001);
         topClaw.setPosition(0.16);
         bottomClaw.setPosition(0.617);
-        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+24 + 4 * TILE_INCH-7+12-24, 47.25-17.5+11.8, maxPwr, 270, 1.2,0.0025,0,0.0001);
+        goToPositionSupreme(-(SKYSTONE_INCH * 2 + 10)+24 + 4 * TILE_INCH-7+12-24, 47.25-17.5+11.8, maxPwr, 270, 1.5,0.0021,0,0.0001);
         //foundation
         ls.setPosition(0.5);
         rs.setPosition(0.5);
